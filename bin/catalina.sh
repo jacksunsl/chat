4e76631a6f4a27bf662f9943e16bea54be0de5af
sl <sunlei@eptok.com>
2017-04-19 15:36:53 +0800

    修改启动脚本

--- bin/catalina.sh
+++ bin/catalina.sh
@@ -1,34 +1,35 @@
 #! /bin/sh
 SERVER_NAME=hipishare-chat.jar
+JAVA_OPTS='-Xms128m -Xmx1024m'
 SERVER_DIR=$(pwd)
-echo $SERVER_DIR
 cd $SERVER_DIR
+
 # start service
 start(){
-        java -Xms128m -Xmx2048m -jar $SERVER_DIR/$SERVER_NAME 5 > start.log &
-        tail -f start.log
+	java $JAVA_OPTS -jar $SERVER_DIR/$SERVER_NAME 2>&1 &
 }
+
 # stop service
 stop(){
-        ps -ef|grep $SERVER_NAME|awk '{print $2}'|while read pid
-        do
-           kill -9 $pid
-        done
+	ps -ef|grep $SERVER_NAME|grep -v grep|awk '{print $2}'|while read pid
+	do
+		kill -9 $pid & printf '['$SERVER_NAME']服务已经停止.\n'
+	done
 }
 
 case "$1" in
 start)
-  start
-  ;;
+	start
+	;;
 stop)
-  stop
-  ;;
+	stop
+	;;
 restart)
-  stop
-  start
-  ;;
+	stop
+	start
+	;;
 *)
-  printf 'Usage: %s {start|stop|restart}\n' "$prog"
-  exit 1
-  ;;
+	printf 'Usage: %s {start|stop|restart}\n' "$prog"
+	exit 1
+	;;
 esac
